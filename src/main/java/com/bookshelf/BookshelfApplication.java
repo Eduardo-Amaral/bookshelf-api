285b07019df4854718b3b320847b922bdd8bfad1
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-04-13 14:56:31 -0300

    H2 server ok

--- src/main/java/com/bookshelf/BookshelfApplication.java
+++ src/main/java/com/bookshelf/BookshelfApplication.java
@@ -1,13 +1,41 @@
 package com.bookshelf;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bookshelf.domain.Book;
+import com.bookshelf.domain.Category;
+import com.bookshelf.repositories.BookRepository;
+import com.bookshelf.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class BookshelfApplication {
+public class BookshelfApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private BookRepository bookRepository;
+	
 
     public static void main(String[] args) {
         SpringApplication.run(BookshelfApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null,"Informatica", "Livros de TI");
+		
+		Book b1 = new Book(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getBooks().addAll(Arrays.asList(b1));
+		
+		this.categoryRepository.saveAll(Arrays.asList(cat1));
+		this.bookRepository.saveAll(Arrays.asList(b1));
+		
+	}
 
 }
--- src/main/java/com/bookshelf/domain/Category.java
+++ src/main/java/com/bookshelf/domain/Category.java
@@ -1,6 +1,11 @@
 package com.bookshelf.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +17,13 @@ public class Category implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
     private String name;
     private String description;
 
+    @OneToMany(mappedBy = "category")
     private List<Book> books = new ArrayList<>();
 
     public Category() {
