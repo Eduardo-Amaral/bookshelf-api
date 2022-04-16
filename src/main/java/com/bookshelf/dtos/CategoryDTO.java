258516bcbe05685e677f5446ec068e7b65f3001b
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-04-16 15:27:52 -0300

    POST category

--- src/main/java/com/bookshelf/dtos/CategoryDTO.java
+++ src/main/java/com/bookshelf/dtos/CategoryDTO.java
@@ -4,40 +4,47 @@ import java.io.Serializable;
 
 import com.bookshelf.domain.Category;
 
-public class CategoryDTO implements Serializable{
+public class CategoryDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
-	private String none;
+	private String name;
 	private String description;
+
 	public CategoryDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public CategoryDTO(Category obj) {
 		super();
 		this.id = obj.getId();
-		this.none = obj.getName();
+		this.name = obj.getName();
 		this.description = obj.getDescription();
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getNone() {
-		return none;
+
+	public String getName() {
+		return name;
 	}
-	public void setNone(String none) {
-		this.none = none;
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
+
 }
